4f281d41b12eabb2d739cc19a7d19c500e45bb11
AldoRCL <aldo_06_05@hotmail.com>
2021-03-15 20:02:38 -0500

    Mi primer codigo javascript

---
+++ Semana4/Dia1/variables/codigo.js
@@ -0,0 +1,17 @@
+// este es un comentario de una linea
+
+
+//variable tipo texto
+var nombre ="Aldo";
+let sistemaOperativo = "linux";
+console.log (typeof nombre)//esto me devuelve string
+/*
+alert(
+    "Mi segundo nombre es" + nombre +
+    "y mi sistema operativo es " + sistemaOperativo
+);*/
+console.log("2"*3)
+console.log("1"+3)
+alert(
+    `Misegundo nombre es ${nombre} y mi sistema operativo es ${sistemaOperativo} `
+)
