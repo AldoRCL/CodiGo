bef63b0597d51a29fdc0f143ca894a1b9a75eee9
AldoRCL <aldo_06_05@hotmail.com>
2021-03-16 19:25:32 -0500

    condicionales

---
+++ Semana4/Dia2/condicionales/codigo.js
@@ -0,0 +1,47 @@
+
+
+//Condicionales
+/*
+if(se cumple mi condicion){
+
+}else(si no se cumple)
+*/
+/*
+let condicion = true;
+
+if(condicion == true){
+    console.log("Hola amigos, bienvenidos a CodiGo")
+}else{
+    console.log("Que estas esperando")
+}
+
+let a =0;
+let b = 0;
+
+a=4;
+//b=5;
+ b = Math.round(Math.random()*10);
+alert("Número aleatorio entre 0 y 10:"+ b)
+
+let suma = a + b;
+
+if(suma === 9){
+    alert("FELICIDADES, ADIVINASTE!!!")
+}else{
+    alert("OH, LOSIENTO, SIGUE INTENTANDO")
+}
+*/
+
+let lenguaje_de_programacion = "c++"
+
+if(lenguaje_de_programacion == "python"){
+    console.log("Python es asombroso")
+}else if(lenguaje_de_programacion == "javascript"){
+    console.log("Javascript tambien lo es")
+}else if(lenguaje_de_programacion == "php"){
+    console.log("Mejor apaga tu computadora amigo/a ")
+}else if(lenguaje_de_programacion == "c++"){
+    console.log("wow tu eres de los bravos")
+}else{
+    console.log("que? aun no te sabes ninguno ? ñ.ñ")
+}
---
+++ pruebac++/src/ejemplo.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"Hola munda"<<endl;
+    return 0;
+}
