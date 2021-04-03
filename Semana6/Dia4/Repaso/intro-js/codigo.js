4c91c012c2faffc707aaf432c2ac1a32c05459ec
AldoRCL <79492243+AldoRCL@users.noreply.github.com>
2021-04-03 12:54:01 -0500

    clase sabado

---
+++ Semana6/Dia4/Repaso/intro-js/codigo.js
@@ -0,0 +1,62 @@
+/*
+//VARIABLES
+
+let variable_texto = "cadena de texto"
+
+let variable_numerica_entera = 0
+
+let variable_numerica_decimal = 6.5
+
+let variable_boobleana_verdadera = true
+
+let variable_boobleana_falsa = false
+
+//ARRAYS
+
+let variable_array = [1,2,3,4]
+let variable_funcion = function sumar(a,b){return a+b}
+
+let variable_objeto = {
+    propiedad1: "cadena de texto",
+    propiedad2: 0,
+    propiedad3:{
+        subpropiedad1:"cadena de texto",
+        subpropiedad2:2
+    },
+    propiedad4: false
+}
+
+//COSNTANTES
+
+const miNombre = "Mi nombre no cambiara con el tiempo"
+
+const miFechaCumple= new Date("May 06 2001")
+
+const DiaActual = new Date()
+
+let DiaActual = new Date()
+
+
+//OPERACIONES ARITMETICAS
+
+let suma
+let resta
+let multiplicacion
+let division
+let modulo
+
+let precedencia_aritemetica
+*/
+let a = 3
+let b = 4
+let binomio = a**2+(2*a*b)+b**2;
+
+console.log(binomio);
+
+
+let nombre = "Aldo"
+
+let evaluaCondicion = nombre == "Aldo" ? "Si se llama asi " : "Hey!, te equivocaste de nombre";
+
+
+ let 
