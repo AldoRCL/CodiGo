82ef2a1e7f920486728787b322729bdc905b1929
AldoRCL <79492243+AldoRCL@users.noreply.github.com>
2021-04-05 21:33:34 -0500

    semana7

---
+++ Semana7/Asincronia/codigo.js
@@ -0,0 +1,35 @@
+
+// lista  de que hacer
+
+//1.lavar la ropa
+//2.pasear al perro
+//3.sacar la basura
+//cocinar algo
+
+function lavarRopa () {
+    console.log("lavar ropa")
+}
+function pasearAlperro () {
+    console.log("pasear al perro")
+}
+function sacarLaBasura () {
+    console.log("sacar la basura")
+}
+function cocinarAlgo () {
+    console.log("cocinar algo")
+}
+
+
+function quehaceres (){
+    lavarRopa();
+    pasearAlperro();
+    sacarLaBasura();
+    cocinarAlgo();
+}
+
+function quehaceres2 (callback){
+    callback(err,lavarRopa);
+    pasearAlperro();
+}
+
+ console.log(quehaceres())
