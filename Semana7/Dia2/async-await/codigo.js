7331d41276ebc237c733c9f3035c0d2de4f5447e
AldoRCL <79492243+AldoRCL@users.noreply.github.com>
2021-04-06 18:24:23 -0500

    Trabajando async

---
+++ Semana7/Dia2/async-await/codigo.js
@@ -0,0 +1,14 @@
+
+//funcion asincrona
+async function fun_asincrona(){
+    return 23
+}
+
+//funcion flecha asincrona
+const otraFuncion = async() => 42;
+
+console.log(fun_asincrona())
+
+fun_asincrona().then(valor=>{
+    console.log("Este es el valor de la promesa",valor)
+})
